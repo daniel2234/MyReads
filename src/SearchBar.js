890f4f7d41384a458098900b85df1054ee8fed2b
daniel2234 <daniel2234@me.com>
2017-08-31 14:18:40 -0400

    configured  proper routing

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react'
-
+import {Link} from 'react-router-dom'
 
 class SearchBar extends Component {
   render() {
     return (
           <div className="search-books">
             <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+              <Link className="close-search" to="/"/>
               <div className="search-books-input-wrapper">
                 {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
